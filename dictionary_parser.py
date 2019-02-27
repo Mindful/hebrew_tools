e41f674e982cafa0139d8dd43e67d22bdf428b74
Joshua Tanner <jotanner@ebay.com>
2019-02-27 17:05:02 +0900

    first draft working implementation

--- dictionary_parser.py
+++ dictionary_parser.py
@@ -2,11 +2,10 @@ import xml.etree.ElementTree
 import logging
 import unicodedata
 
-
 SCHEMA_PREFIX = '{http://openscriptures.github.com/morphhb/namespace}'
-ENTRY_TYPE = SCHEMA_PREFIX +'entry'
+ENTRY_TYPE = SCHEMA_PREFIX + 'entry'
 
-ENTRY_POS = SCHEMA_PREFIX +'pos'
+ENTRY_POS = SCHEMA_PREFIX + 'pos'
 ENTRY_DEFINITION = SCHEMA_PREFIX + 'def'
 ENTRY_WORD = SCHEMA_PREFIX + 'w'
 
@@ -17,69 +16,92 @@ LOGGER_NAME = 'hdict'
 
 
 def strip_nikud_vowels(string):
-	normalized=unicodedata.normalize('NFKD', string)
-	return ''.join([c for c in normalized if not unicodedata.combining(c)])
+    normalized = unicodedata.normalize('NFKD', string)
+    return ''.join([c for c in normalized if not unicodedata.combining(c)])
+
 
 class DictionaryParseException(Exception):
-	pass
+    pass
+
 
 class DictionaryEntry(object):
-	__slots__ = ('dict_id', 'word', 'word_vowelless', 'pronunciation', 'meaning', 'pos')
+    __slots__ = ('dict_id', 'word', 'word_vowelless', 'pronunciation', 'meaning', 'pos')
 
-	def __init__(self, dict_id, word, pronunciation, meaning, pos):
-		self.dict_id = dict_id
-		self.word = word
-		self.word_vowelless = strip_nikud_vowels(word)
-		self.pronunciation = pronunciation
-		self.meaning = meaning
-		self.pos = pos
+    def __init__(self, dict_id, word, meaning, pos):
+        for attr in self.__slots__:
+            setattr(self, attr, None)
+        self.dict_id = dict_id
+        if word is not None:
+            self.word = word.text
+            self.word_vowelless = strip_nikud_vowels(self.word)
+            self.pronunciation = word.attrib[WORD_PRONUNCIATION]
+        if meaning is not None:
+            self.meaning = meaning.text.lower()
+        if pos is not None:
+            self.pos = pos.text
 
-	def dict_key(self):
-		return (self.word_vowelless, self.meaning)
+    def dict_key(self):
+        if self.word_vowelless and self.meaning:
+            return (self.word_vowelless, self.meaning)
+        else:
+            return None
+
+    def missing_attributes(self):
+        return [attr for attr in self.__slots__ if getattr(self, attr) is None]
+
+    def is_complete_entry(self):
+        return len(self.missing_attributes()) == 0
 
 
 class DictionaryParser(object):
-	def __init__(self, file_name):
-		self.file_name = file_name
-		self.logger = logging.getLogger(LOGGER_NAME)
-		self.dictionary = self.construct_dictionary()
+    def __init__(self, file_name):
+        self.file_name = file_name
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+        self.logger = logging.getLogger(LOGGER_NAME)
+        self.dictionary = self.construct_dictionary()
 
+    def construct_dictionary(self):
+        tree = xml.etree.ElementTree.parse(self.file_name)
+        root = tree.getroot()
+        dictionary = {}
+        entry_counter = 0
+        for entry in root.iter('{http://openscriptures.github.com/morphhb/namespace}entry'):
+            entry_object = self.processEntry(entry)
+            entry_counter += 1
+            if entry_object:
+                key = entry_object.dict_key()
+                if key:
+                    if key in dictionary:
+                        self.logger.error(str(key) + ' encountered twice')
+                    else:
+                        dictionary[key] = entry_object
+                else:
+                    self.logger.error("Could not generate key for entry with id "+entry_object.dict_id)
 
-	def construct_dictionary(self):
-		tree = xml.etree.ElementTree.parse(self.file_name)
-		root = tree.getroot()
-		dictionary = {}
-		for entry in root.iter('{http://openscriptures.github.com/morphhb/namespace}entry'):
-			entry_object = self.processEntry(entry)
-			if entry_object:
-				key = entry_object.dict_key()
-				if key in dictionary:
-					raise DictionaryParseException(str(key) + ' encountered twice')
-				else:
-					dictionary[key] = entry_object
+        self.logger.info("Scanned " + str(entry_counter) + " raw entries")
+        complete_entries = sum(1 for entry in dictionary.values() if entry.is_complete_entry())
+        self.logger.info("Generated " + str(len(dictionary)) + " dictionary entries")
+        return dictionary
 
-		return dictionary
+    def processEntry(self, entry):
+        dict_id = entry.attrib[ENTRY_ID]
 
+        word = entry.find(ENTRY_WORD)
+        pos = entry.find(ENTRY_POS)
+        definition = entry.find(ENTRY_DEFINITION)
 
+        for sub_element in [(word, 'word'), (pos, 'part of speech'), (definition, 'definition')]:
+            if sub_element[0] is None:
+                self.logger.warning('Could not find ' + sub_element[1] + ' for dictionary entry with id ' + dict_id)
 
-	def processEntry(self, entry):
-		dict_id = entry.attrib[ENTRY_ID]
-
-		word = entry.find(ENTRY_WORD)
-		pos = entry.find(ENTRY_POS)
-		definition = entry.find(ENTRY_DEFINITION)
-
-		for sub_element in [(word, 'word'), (pos, 'part of speech'), (definition, 'definition')]:
-			if sub_element[0] is None:
-				self.logger.warn('Could not find '+sub_element[1]+' for dictionary entry with id '+dict_id)
-				return None
-
-
-		result = DictionaryEntry(dict_id, word.text, word.attrib[WORD_PRONUNCIATION], pos.text, definition.text)
-		return result
+        result = DictionaryEntry(dict_id, word, definition, pos)
+        return result
 
 
 def get_dict():
-	parser = DictionaryParser('LexicalIndex.xml')
-	return parser.dictionary
+    parser = DictionaryParser('LexicalIndex.xml')
+    return parser.dictionary
 
+
+if __name__ == '__main__':
+    get_dict()
