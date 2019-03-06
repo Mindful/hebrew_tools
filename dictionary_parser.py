8909769b71f1c7ab47a84162f6500860bcd210ca
Joshua Tanner <jotanner@ebay.com>
2019-03-05 16:22:40 -0800

    start bdb lookup work

--- dictionary_parser.py
+++ dictionary_parser.py
@@ -3,17 +3,26 @@ import logging
 import unicodedata
 from collections import defaultdict
 
+LEXICAL_INDEX = 'LexicalIndex.xml'
+BROWN_DRIVER_BRIGGS = 'BrownDriverBriggs.xml'
+
 SCHEMA_PREFIX = '{http://openscriptures.github.com/morphhb/namespace}'
 ENTRY_TYPE = SCHEMA_PREFIX + 'entry'
+PART_TYPE = SCHEMA_PREFIX + 'part'
+
+PART_LANGUAGE = '{http://www.w3.org/XML/1998/namespace}lang'
 
 ENTRY_POS = SCHEMA_PREFIX + 'pos'
 ENTRY_DEFINITION = SCHEMA_PREFIX + 'def'
 ENTRY_WORD = SCHEMA_PREFIX + 'w'
+ENTRY_XREF = SCHEMA_PREFIX + 'xref'
 
 WORD_PRONUNCIATION = 'xlit'
 ENTRY_ID = 'id'
 
 LOGGER_NAME = 'hdict'
+
+#TODO: We could get gender information for some nouns from BrownDriverBriggss
 
 
 def strip_nikud_vowels(string):
@@ -26,9 +35,9 @@ class DictionaryParseException(Exception):
 
 
 class DictionaryEntry(object):
-    __slots__ = ('dict_id', 'word', 'word_vowelless', 'pronunciation', 'meaning', 'pos')
+    __slots__ = ('dict_id', 'word', 'word_vowelless', 'pronunciation', 'meaning', 'pos', 'gender')
 
-    def __init__(self, dict_id, word, meaning, pos):
+    def __init__(self, dict_id, word, meaning, pos, gender):
         for attr in self.__slots__:
             setattr(self, attr, None)
         self.dict_id = dict_id
@@ -40,6 +49,7 @@ class DictionaryEntry(object):
             self.meaning = meaning.text.lower()
         if pos is not None:
             self.pos = pos.text
+        #TODO: gender
 
     def dict_key(self):
         return self.word_vowelless
@@ -52,19 +62,33 @@ class DictionaryEntry(object):
 
 
 class DictionaryParser(object):
-    def __init__(self, file_name):
-        self.file_name = file_name
+    def __init__(self):
         logging.basicConfig(level=logging.INFO, format='%(message)s')
         self.logger = logging.getLogger(LOGGER_NAME)
         self.dictionary = self.construct_dictionary()
 
+    def process_bdb(self):
+        tree = xml.etree.ElementTree.parse(BROWN_DRIVER_BRIGGS)
+        hebrew_parts = (part for part in tree.getroot().iter(PART_TYPE) if part.attrib[PART_LANGUAGE] == 'heb')
+        dictionary = {}
+        for part in hebrew_parts:
+            for entry in part.iter(ENTRY_TYPE):
+                entry_id = entry.attrib[ENTRY_ID]
+                if entry_id in dictionary:
+                    self.logger.warning("Duplicate BDB entry with id "+id)
+                dictionary[entry.attrib[ENTRY_ID]] = entry
+
+        return dictionary
+
+
     def construct_dictionary(self):
-        tree = xml.etree.ElementTree.parse(self.file_name)
-        root = next(part for part in tree.getroot().iter('{http://openscriptures.github.com/morphhb/namespace}part') if
-                    part.attrib['{http://www.w3.org/XML/1998/namespace}lang'] == 'heb')
+        self.brown_driver_briggs = self.process_bdb()
+
+        tree = xml.etree.ElementTree.parse(LEXICAL_INDEX)
+        hebrew_root = next(part for part in tree.getroot().iter(PART_TYPE) if part.attrib[PART_LANGUAGE] == 'heb')
         dictionary = defaultdict(list)
         entry_counter = 0
-        for entry in root.iter('{http://openscriptures.github.com/morphhb/namespace}entry'):
+        for entry in hebrew_root.iter(ENTRY_TYPE):
             entry_object = self.processEntry(entry)
             entry_counter += 1
             if entry_object:
@@ -84,17 +108,26 @@ class DictionaryParser(object):
         word = entry.find(ENTRY_WORD)
         pos = entry.find(ENTRY_POS)
         definition = entry.find(ENTRY_DEFINITION)
+        xref = entry.find(ENTRY_XREF)
 
-        for sub_element in [(word, 'word'), (pos, 'part of speech'), (definition, 'definition')]:
+        for sub_element in [(word, 'word'), (pos, 'part of speech'), (definition, 'definition'),
+                            (xref, 'external references')]:
             if sub_element[0] is None:
                 self.logger.warning('Could not find ' + sub_element[1] + ' for dictionary entry with id ' + dict_id)
 
-        result = DictionaryEntry(dict_id, word, definition, pos)
+        if xref is not None and xref.attrib.get('bdb') is not None:
+            bdb_entry = self.brown_driver_briggs[xref.attrib['bdb']]
+            bdb_pos = bdb_entry.find(ENTRY_POS)
+            if bdb_pos is not None:
+                print("derpy")
+
+
+        result = DictionaryEntry(dict_id, word, definition, pos, xref.attrib)
         return result
 
 
 def get_dict():
-    parser = DictionaryParser('LexicalIndex.xml')
+    parser = DictionaryParser()
     return parser.dictionary
 
 
